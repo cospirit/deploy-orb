217c7f56dfa3f96a3eaec33d3958f6ccbbc10a41
Spoon4 <spoon4@gmail.com>
2021-01-19 04:46:14 +0100

    chore: renamed env var for k8s service conf file

--- src/scripts/k8s_service_version.sh
+++ src/scripts/k8s_service_version.sh
@@ -1,6 +1,6 @@
 update_service_version() {
-	sed -i "0,/version:.*$/ s/version:.*$/version: ${VERSION}/" "${K8S_CONF_FILE}"
-    head -n 3 "${K8S_CONF_FILE}"
+	sed -i "0,/version:.*$/ s/version:.*$/version: ${VERSION}/" "${SERVICE_CONF_FILE}"
+    head -n 3 "${SERVICE_CONF_FILE}"
 }
 
 # Will not run if sourced for bats-core tests.
