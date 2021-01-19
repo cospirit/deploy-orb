890a4e0761b18d0488b3f3a95517ba59bbab4894
Spoon4 <spoon4@gmail.com>
2021-01-19 06:21:30 +0100

    fix: git_push remote missed

--- src/scripts/git_push.sh
+++ src/scripts/git_push.sh
@@ -1,9 +1,9 @@
 git_push() {
-	git config --global user.email "${GIT_USER_EMAIL}"
+    git config --global user.email "${GIT_USER_EMAIL}"
     git config --global user.name "${GIT_USER_NAME}"
     git pull origin "$3"
     git commit -m "$1 [skip ci]" "$2"
-    git push "$3"
+    git push origin "$3"
 }
 
 # Will not run if sourced for bats-core tests.
